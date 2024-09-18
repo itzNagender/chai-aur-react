60eb3b952614be4237a48ef1327db1eb5453cab3
nagender811 <kumarnagender811@gmail.com>
2024-09-18 17:27:41 +0530

    add modified index.js

--- 012megaBlog/src/components/index.js
+++ 012megaBlog/src/components/index.js
@@ -1,8 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container"
+import Container from "./container/Container";
 import Logo from "./Logo";
-import LogoutBtn from "./Header/LogoutBtn"
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard"
 
 export {
     Header,
@@ -10,4 +15,9 @@ export {
     Container,
     Logo,
     LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
 }
