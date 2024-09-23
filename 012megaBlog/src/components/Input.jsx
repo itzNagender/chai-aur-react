fc4383a890afc051b3363cae2e32111a1ce68449
nagender811 <kumarnagender811@gmail.com>
2024-09-23 19:04:11 +0530

    add modified pages

--- 012megaBlog/src/components/Input.jsx
+++ 012megaBlog/src/components/Input.jsx
@@ -1,6 +1,7 @@
 import React, { useId } from 'react'
 
 const Input = React.forwardRef( function Input ({
+    label,
     type = "text",
     className = "",
     ...props
@@ -18,7 +19,7 @@ const Input = React.forwardRef( function Input ({
                 type={type}
                 className= {`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
                 ref={ref}
-                ${...props}
+                {...props}
                 id={id}
             />
         </div>
